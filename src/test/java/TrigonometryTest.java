ccdb2b06ca702bb09fb51422f4b7d55928630115
Roman Glinskikh <glinskikh.roman@yandex.ru>
2023-03-17 15:41:41 +0300

    add trig mock in ExpressionTest

--- src/test/java/TrigonometryTest.java
+++ src/test/java/TrigonometryTest.java
@@ -5,23 +5,6 @@ import org.junit.jupiter.params.provider.ValueSource;
 import static org.mockito.Mockito.*;
 import static org.junit.jupiter.api.Assertions.*;
 
-class MathExtension {
-    public static double sec(final double x) {
-        if (Math.abs(Math.cos(x)) < 1E-3) {
-            return Double.POSITIVE_INFINITY;
-        }
-
-        return 1 / Math.cos(x);
-    }
-    public static double csc(final double x) {
-        if (Math.abs(Math.sin(x)) < 1E-3) {
-            return Double.POSITIVE_INFINITY;
-        }
-
-        return 1 / Math.sin(x);
-    }
-}
-
 public class TrigonometryTest {
 
     @BeforeAll
